6d7cd8f04e2df16ca6abeef54d15a40407efcac6
Fatih Mehmet Varli <fath.varl@gmail.com>
2023-12-21 17:31:15 +0300

    remove unused import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
 'use client'
 
 import {usePostService} from "@/services/usePostService";
-import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
 
-const queryClient = new QueryClient()
 export default function Home() {
 
     const { data , isLoading , isSuccess } = usePostService();
